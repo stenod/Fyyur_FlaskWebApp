5dfc8b23b01a40757e7ba54b3122d7a072043a46
Stephan <s0544659@htw-berlin.de>
2020-04-09 17:23:02 +0200

    - added edit venue backend and view

--- app.py
+++ app.py
@@ -80,6 +80,8 @@ class Artist(db.Model):
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
+    seeking_venue = db.Column(db.BOOLEAN, nullable=False, default=False)
+    seeking_description = db.Column(db.String(300))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     upcoming_shows = db.relationship('Show', primaryjoin="and_(Artist.id==Show.artist_id, "
@@ -308,38 +310,49 @@ def edit_artist(artist_id):
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-    # TODO: take values from the form submitted, and update existing
-    # artist record with ID <artist_id> using the new attributes
-
-    return redirect(url_for('show_artist', artist_id=artist_id))
+    try:
+        artist = Artist.query.get(artist_id)
+        form = ArtistForm(request.form)
+        artist.name = form.name.data
+        artist.state = form.state.data
+        artist.city = form.city.data
+        artist.phone = form.phone.data
+        artist.genres = ','.join(form.genres.data)
+        artist.facebook_link = form.facebook_link.data
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+        return redirect(url_for('show_artist', artist_id=artist_id))
 
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-    form = VenueForm()
-    venue = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
-    # TODO: populate form with values from venue with ID <venue_id>
+    venue = Venue.query.get(venue_id)
+    form = VenueForm(obj=venue)
+    form.genres.data = venue.genres.split(',')
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-    # TODO: take values from the form submitted, and update existing
-    # venue record with ID <venue_id> using the new attributes
-    return redirect(url_for('show_venue', venue_id=venue_id))
+    try:
+        venue = Venue.query.get(venue_id)
+        form = VenueForm(request.form)
+        venue.name = form.name.data
+        venue.state = form.state.data
+        venue.city = form.city.data
+        venue.address = form.address.data
+        venue.phone = form.phone.data
+        venue.genres = ','.join(form.genres.data)
+        venue.facebook_link = form.facebook_link.data
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+        return redirect(url_for('show_venue', venue_id=venue_id))
 
 
 #  Create Artist
