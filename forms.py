2f94a721f5a158f3675437339d1641591d9e382a
Stephan <s0544659@htw-berlin.de>
2020-04-09 18:21:17 +0200

    - added requirements, updated forms

--- forms.py
+++ forms.py
@@ -6,10 +6,10 @@ from wtforms.validators import DataRequired, AnyOf, URL
 
 class ShowForm(Form):
     artist_id = StringField(
-        'artist_id'
+        'artist_id', validators=[DataRequired()]
     )
     venue_id = StringField(
-        'venue_id'
+        'venue_id', validators=[DataRequired()]
     )
     start_time = DateTimeField(
         'start_time',
@@ -91,7 +91,6 @@ class VenueForm(Form):
         'image_link'
     )
     genres = SelectMultipleField(
-        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
         choices=[
             ('Alternative', 'Alternative'),
@@ -191,14 +190,12 @@ class ArtistForm(Form):
         ]
     )
     phone = StringField(
-        # TODO implement validation logic for state
         'phone'
     )
     image_link = StringField(
         'image_link'
     )
     genres = SelectMultipleField(
-        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
         choices=[
             ('Alternative', 'Alternative'),
@@ -223,7 +220,6 @@ class ArtistForm(Form):
         ]
     )
     facebook_link = StringField(
-        # TODO implement enum restriction
         'facebook_link'
     )
     website_link = StringField(
@@ -232,4 +228,3 @@ class ArtistForm(Form):
     seeking_venue = BooleanField('seeking_venue')
     seeking_description = StringField('seeking_description')
 
-# TODO IMPLEMENT NEW ARTIST FORM AND NEW SHOW FORM
