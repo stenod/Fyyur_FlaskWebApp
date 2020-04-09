9cb220f915fda5929357fd9fc0b862dda67bb583
Stephan <s0544659@htw-berlin.de>
2020-04-09 14:59:58 +0200

    - added backend for deleting venue and frontend view changes

--- app.py
+++ app.py
@@ -219,12 +219,22 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-    # TODO: Complete this endpoint for taking a venue_id, and using
     # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
-    print('working')
+    venue = Venue.query.filter_by(id=venue_id)
+    try:
+        venue.delete()
+        db.session.commit()
+        flash('Venue successfully deleted!')
+        return jsonify({'success': True})
+    except:
+        db.session.rollback()
+        flash('Error: Venue could not be deleted deleted!')
+        return jsonify({'success': False}), 400
+    finally:
+        db.session.close()
+
     # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
     # clicking that button delete it from the db then redirect the user to the homepage
-    return 'yes'
 
 
 #  Artists
